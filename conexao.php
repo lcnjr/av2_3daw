b659acc244887558293faf82c31456cbf4caf14c
lcnjunior37 <lcnjunior37@gmail.com>
2020-12-19 15:21:53 -0300

    criando a conexao

---
+++ conexao.php
@@ -0,0 +1,26 @@
+<?php
+function createCon(){
+        $servername = "localhost";
+        $username = "";
+        $password = "";
+        $dbname = "av2_3daw";
+
+        // Create connection
+        try {
+            $conn = new mysqli($servername, $username, $password, $dbname);
+        }catch(PDOException $erro){
+            echo "Erro na conexão:" . $erro->getMessage();
+        }
+
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        return $conn;
+    }
+    function closeCon($conn){
+        $conn->close();
+
+    }
+
+?>
